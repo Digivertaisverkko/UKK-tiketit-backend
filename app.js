f10ea6884c09f4805ae264b0ce44652939a890c5
Henri K <12832691+nkahe@users.noreply.github.com>
2022-10-03 15:11:47 +0300

    CORS-paketin lisäys ja käyttöön asettaminen, jotta voidaan localhostissa lähettää kutsuja eri porteissa olevan frontin ja backin välillä.

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+const cors = require('cors');
+app.use(cors());
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
