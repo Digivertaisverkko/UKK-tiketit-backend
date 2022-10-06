d32dae2d453fcad0631f334ff77f0a344910378f
Joni Rajala <jonimatiasrajala@gmail.com>
2022-10-06 13:35:59 +0300

    Hakuja SQL-tietokantaan. Ei vielä lisäyksiä.

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var sqlSite = require('./routes/sql')
 
 var app = express();
 
