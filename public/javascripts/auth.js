9573142a3b29bdb6d8457ac74c1c7e0439d9debc
Joni Rajala <jonimatiasrajala@gmail.com>
2022-10-13 13:42:03 +0300

    Sisäänkirjautumisen ensimmäiset kaksi vaihetta (api/login ja api/omalogin) toteutettu. README muutoksia.

--- public/javascripts/auth.js
+++ public/javascripts/auth.js
@@ -3,24 +3,44 @@ const crypto = require('crypto');
 const sql = require('mysql');
 
 var authsBySession = new Object();
-
-var loginRequestsByCodeChallenge = new Object();
+var sessionsByLogin = new Object();
 
 module.exports = {
 
-    createLoginPage: function() {
-
+    sqlLoginStart: function(codeChallenge) {
+        return new Promise(function(resolve, reject) {
+            let loginid = crypto.randomUUID();
+            let fronttunnus = crypto.randomUUID();
+            sql.createLoginUrl(loginid, codeChallenge, fronttunnus).then((data) => {
+                resolve(data);
+            });
+        });
     },
 
     startLogin: function(codeChallenge, logintype) {
-
+        return new Promise(function(resolve, reject) {
+            if (logintype == 'own') {
+                return sqlLoginStart(codeChallenge).then((data) => {resolve(data)});
+            }
+        });        
     },
 
-    login: function(username, password) {
+    login: function(username, password, loginid) {
         return new Promise(function(resolve, reject) {
-            let sessionId = crypto.randomUUID();
-            authsBySession[sessionId] = authCode;
-            resolve(json({ idtoken: '', sessionid: sessionId}))
+            sql.getSalt(username).then((saltData) => {
+
+                let hash = crypto.createHash('sha256', password).update(salt).digest('hex');
+
+                sql.checkUserAccount(username, hash).then((accountData) => {
+                    if (data2 != null) {
+                        sql.updateLoginAttemptWithAccount(loginid, accountData.tili).then((updateData) => {
+                            sql.getLoginAttemptWithId(loginid).then((attemptData) => {
+                                resolve(json({success: true, 'login-code': attemptData.fronttunnus}));
+                            });
+                        });
+                    }
+                });
+            });
         });
     },
 
--- routes/sql.js
+++ routes/sql.js
@@ -1,9 +1,86 @@
-
 var mysql = require('mysql');
 const { use } = require('.');
 
-
 module.exports = {
+
+  createLoginUrl: function(loginid, codeChallenge, frontcode) {
+    return new Promise(function (resolve, reject) {
+      const query = 'INSERT INTO LoginYritys (loginid, codeChallenge, fronttunnus) VALUES (?, ?, ?)';
+      con.query(query, [loginid, codeChallenge, frontcode], function(err, rows, fields) {
+        if (err) {
+          return reject(err);
+        }
+        resolve('/login?loginid=' + loginid);
+      });
+    });
+  },
+
+  updateLoginAttemptWithAccount: function(loginid, userid) {
+    return new Promise( function (resolve, reject) {
+      const query = 'UPDATE LoginYritys SET tili=? WHERE loginid=?';
+      con.query(query, [userid, loginid], function(err, rows, fields) {
+        if (err) {
+          return reject(err);
+        }
+        resolve(rows);
+      });
+    });
+  },
+
+  getLoginAttemptWithId: function(loginid) {
+    return new Promise( function (resolve, reject) {
+      const query = 'SELECT * FROM LoginYritys WHERE loginid=?';
+      con.query(query, [loginid], function(err, rows, fields) {
+        if (err) {
+          return reject(err);
+        }
+        resolve(rows);
+      });
+    });
+  },
+
+  getSalt: function(username) {
+    return new Promise(function(resolve, reject) {
+      const query = 'SELECT salt FROM Login WHERE ktunnus=?';
+      con.query(query, [username], function(err, rows, fields) {
+        if (err) {
+          return reject(err);
+        }
+        resolve(rows);
+      });
+    });
+  },
+
+  checkUserAccount: function(username, passwordhash) {
+    return new Promise(function(resolve, reject) {
+      const query = 'SELECT * FROM Login WHERE ktunnus=? AND salasana=?';
+      con.query(query, [username, passwordhash], function(err, rows, fields) {
+        if (err) {
+          return reject(err);
+        }
+        resolve(rows);
+      });
+    });
+  },
+
+  createAccount: function(username, passwordhash, salt) {
+    return new Promise(function(resolve, reject) {
+      const query = 'INSERT INTO Login (ktunnus, salasana, salt) VALUES (?, ?, ?)';
+      con.query(query, [username, passwordhash, salt], function(err, rows, fields) {
+        if (err) {
+          return reject(err);
+        }
+        resolve(rows);
+      });
+    });
+  },
+
+
+
+
+
+
+
 
 
   getAllCourses: function() {
