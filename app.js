1f91eed5aea11ffca91c6bf08797605df781b4b2
Erkki Tervo <erkki.tervo@oulu.fi>
2023-04-03 09:39:52 +0300

    Ei käytetä expressjs:n omaa näkymää error sivulle

--- app.js
+++ app.js
@@ -125,9 +125,8 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // send error status
   res.status(err.status || 500);
-  res.render('error');
 });
 
 app.listen(port);
