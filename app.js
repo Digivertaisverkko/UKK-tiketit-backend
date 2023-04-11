1558f10d48ff9e65d374204bc8c08ffb6295dc53
Erkki Tervo <erkki.tervo@oulu.fi>
2023-04-11 15:20:31 +0300

    Muutetaan FRONTEND_DIRECTORY ympäristömuuttujan oletusasetukseksi ./static

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ const access = require('./public/javascripts/access management/access.js')
 app.use(cors());
 
 const port = process.env.PORT || 3000;
-const frontendDirectory = process.env.FRONTEND_DIRECTORY || __dirname + '/UKK-tiketit/dist/tikettisysteemi/';
+const frontendDirectory = process.env.FRONTEND_DIRECTORY || __dirname + '/static/';
 
 cron.schedule('0 4 * * *', () => {
   timedJobs.archiveOldTickets();
