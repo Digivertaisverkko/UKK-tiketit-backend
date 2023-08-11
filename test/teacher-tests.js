ca91b222198b1f1eae3a23b0090321c12db9e002
Joni Rajala <jonimatiasrajala@gmail.com>
2023-08-11 15:49:36 +0300

    Automaattitestit ukk:iden viennille ja tuonnille.

--- test/teacher-tests.js
+++ test/teacher-tests.js
@@ -7,6 +7,7 @@ const { forEach } = require("jszip");
 const arrayTools = require("../public/javascripts/arrayTools.js");
 const testhelpers = require("../testhelpers/testhelpers.js");
 const allrolesTests = require("../testhelpers/allroles-tests.js");
+const testobjects = require('../testhelpers/test-objects.js');
 
 chai.use(chaiHttp);
 const expect = chai.expect;
@@ -40,6 +41,45 @@ describe('Opettajan oikeuksien testaaminen', function() {
     allrolesTests.updateTicketBaseSuccessfullyTest(teacherAgent, 'opettaja', 1);
     allrolesTests.fetchTicketBaseUnsuccessfullyTest(teacherAgent, 'opettaja', 6);
     allrolesTests.updateTicketBaseUnsuccessfullyTest(teacherAgent, 'opettaja (vääräkurssi)', 6);
+  });
+
+
+  describe('Opettajan tiketin vienti ja tuonti', function() {
+
+    it('hakee tuotavat ukk-tiedot palvelimelta', function(done) {
+      teacherAgent.get('/api/kurssi/1/ukk/vienti')
+      .send({})
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body).to.be.an('array');
+        res.body.forEach(bodyElement => {
+          expect(bodyElement).to.have.all.keys(['id', 'otsikko', 'aikaleima', 'tila', 'kommentit', 'kentat']);
+          expect(bodyElement.kommentit).to.be.an('array');
+          expect(bodyElement.kentat).to.be.an('array');
+          bodyElement.kommentit.forEach(element => {
+            expect(element).to.have.all.keys(['id', 'tiketti', 'lahettaja', 'viesti', 'aikaleima', 'muokattu', 'tila']);
+          });
+          bodyElement.kentat.forEach(element => {
+            expect(element).to.have.all.keys(['tiketti', 'arvo', 'otsikko', 'tyyppi', 'ohje']);
+          });
+          done();
+        });
+      });
+    });
+
+    it('lähettää tuodut tiketin palvelimelle', function(done) {
+      teacherAgent.post('/api/kurssi/1/ukk/vienti')
+      .send(testobjects.exportJson)
+      .end((err, res) => {
+        console.dir(res.body);
+        expect(res).to.have.status(200);
+        expect(res.body).to.have.all.keys(['success']);
+        expect(res.body.success).to.be.true;
+        done();
+      });
+  
+    });
+    
   });
 
 });
---
+++ testhelpers/test-objects.js
@@ -0,0 +1,97 @@
+
+
+
+module.exports = {
+
+  exportJson: [
+    {
+      "id": 7,
+      "otsikko": "UKK kysymys",
+      "aikaleima": "2023-05-12T08:59:47.245Z",
+      "tila": 1,
+      "kommentit": [
+        {
+          "id": 11,
+          "tiketti": 7,
+          "lahettaja": 3,
+          "viesti": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sodales convallis purus. Etiam dui eros, vestibulum tincidunt lacus sed, pharetra luctus Leo. Sed nisl mauris, sodales cursus nisi a, mollis varius metus. Pellentesque enim mi, rhoncus eu tellus vitae, gravida porttitor ligula. Pellentesque efficitur vitae lectus pellentesque convallis. Sed et ultricies libero, in vulputate ipsum. Etiam aliquam dignissim sollicitudin. Interdum et malesuada fames ac ante ipsum primis in faucibus. Duis aliquet molestie est, a blandit mauris. Phasellus cursus libero molestie tincidunt mattis. Curabitur eu fermentum metus, suscipit commodo urna. Nulla facilisi. Nam nec efficitur odio, in consectetur felis.<br>Praesent eu ultricies nisl. Phasellus quis nisi eu orci facilisis fermentum et ut ipsum. Integer vitae posuere dolor. Vivamus nec mauris non neque pharetra eleifend. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Curabitur ac consequat est, ut aliquet massa. Morbi rutrum mi sit amet nibh suscipit, ut vulputate quam sollicitudin. Aenean a nibh at sapien porttitor eleifend. Maecenas id justo nec arcu lobortis pretium ut id diam. Mauris maximus auctor mauris. In varius dictum arcu, non eleifend arcu fermentum vel. Proin malesuada elit eros, nec laoreet nisl bibendum at.",
+          "aikaleima": "2023-05-12T08:59:47.260Z",
+          "tila": 1
+        },
+        {
+          "id": 12,
+          "tiketti": 7,
+          "lahettaja": 3,
+          "viesti": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sodales convallis purus. Etiam dui eros, vestibulum tincidunt lacus sed, pharetra luctus Leo. Sed nisl mauris, sodales cursus nisi a, mollis varius metus. Pellentesque enim mi, rhoncus eu tellus vitae, gravida porttitor ligula. Pellentesque efficitur vitae lectus pellentesque convallis. Sed et ultricies libero, in vulputate ipsum. Etiam aliquam dignissim sollicitudin. Interdum et malesuada fames ac ante ipsum primis in faucibus. Duis aliquet molestie est, a blandit mauris. Phasellus cursus libero molestie tincidunt mattis. Curabitur eu fermentum metus, suscipit commodo urna. Nulla facilisi. Nam nec efficitur odio, in consectetur felis.<br>Praesent eu ultricies nisl. Phasellus quis nisi eu orci facilisis fermentum et ut ipsum. Integer vitae posuere dolor. Vivamus nec mauris non neque pharetra eleifend. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Curabitur ac consequat est, ut aliquet massa. Morbi rutrum mi sit amet nibh suscipit, ut vulputate quam sollicitudin. Aenean a nibh at sapien porttitor eleifend. Maecenas id justo nec arcu lobortis pretium ut id diam. Mauris maximus auctor mauris. In varius dictum arcu, non eleifend arcu fermentum vel. Proin malesuada elit eros, nec laoreet nisl bibendum at.",
+          "aikaleima": "2023-05-12T08:59:47.261Z",
+          "tila": 1
+        }
+      ],
+      "kentat": [
+        {
+          "tiketti": 7,
+          "arvo": "2",
+          "otsikko": "Tehtävä",
+          "tyyppi": 1,
+          "ohje": ""
+        },
+        {
+          "tiketti": 7,
+          "arvo": "Aikataulu",
+          "otsikko": "Ongelman tyyppi",
+          "tyyppi": 1,
+          "ohje": ""
+        }
+      ]
+    },
+    {
+      "id": 10,
+      "otsikko": "UKK 1",
+      "aikaleima": "2023-05-13T08:59:47.246Z",
+      "kommentit": [
+        {
+          "id": 13,
+          "tiketti": 10,
+          "lahettaja": 3,
+          "viesti": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sodales convallis purus. Etiam dui eros, vestibulum tincidunt lacus sed, pharetra luctus Leo. Sed nisl mauris, sodales cursus nisi a, mollis varius metus. Pellentesque enim mi, rhoncus eu tellus vitae, gravida porttitor ligula. Pellentesque efficitur vitae lectus pellentesque convallis. Sed et ultricies libero, in vulputate ipsum. Etiam aliquam dignissim sollicitudin. Interdum et malesuada fames ac ante ipsum primis in faucibus. Duis aliquet molestie est, a blandit mauris. Phasellus cursus libero molestie tincidunt mattis. Curabitur eu fermentum metus, suscipit commodo urna. Nulla facilisi. Nam nec efficitur odio, in consectetur felis.<br>Praesent eu ultricies nisl. Phasellus quis nisi eu orci facilisis fermentum et ut ipsum. Integer vitae posuere dolor. Vivamus nec mauris non neque pharetra eleifend. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Curabitur ac consequat est, ut aliquet massa. Morbi rutrum mi sit amet nibh suscipit, ut vulputate quam sollicitudin. Aenean a nibh at sapien porttitor eleifend. Maecenas id justo nec arcu lobortis pretium ut id diam. Mauris maximus auctor mauris. In varius dictum arcu, non eleifend arcu fermentum vel. Proin malesuada elit eros, nec laoreet nisl bibendum at.",
+          "aikaleima": "2023-05-12T08:59:47.261Z",
+          "tila": 1
+        },
+        {
+          "id": 14,
+          "tiketti": 10,
+          "lahettaja": 3,
+          "viesti": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sodales convallis purus. Etiam dui eros, vestibulum tincidunt lacus sed, pharetra luctus Leo. Sed nisl mauris, sodales cursus nisi a, mollis varius metus. Pellentesque enim mi, rhoncus eu tellus vitae, gravida porttitor ligula. Pellentesque efficitur vitae lectus pellentesque convallis. Sed et ultricies libero, in vulputate ipsum. Etiam aliquam dignissim sollicitudin. Interdum et malesuada fames ac ante ipsum primis in faucibus. Duis aliquet molestie est, a blandit mauris. Phasellus cursus libero molestie tincidunt mattis. Curabitur eu fermentum metus, suscipit commodo urna. Nulla facilisi. Nam nec efficitur odio, in consectetur felis.<br>Praesent eu ultricies nisl. Phasellus quis nisi eu orci facilisis fermentum et ut ipsum. Integer vitae posuere dolor. Vivamus nec mauris non neque pharetra eleifend. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Curabitur ac consequat est, ut aliquet massa. Morbi rutrum mi sit amet nibh suscipit, ut vulputate quam sollicitudin. Aenean a nibh at sapien porttitor eleifend. Maecenas id justo nec arcu lobortis pretium ut id diam. Mauris maximus auctor mauris. In varius dictum arcu, non eleifend arcu fermentum vel. Proin malesuada elit eros, nec laoreet nisl bibendum at.",
+          "aikaleima": "2023-05-12T08:59:47.261Z",
+          "tila": 1
+        }
+      ],
+      "kentat": []
+    },
+    {
+      "id": 11,
+      "otsikko": "Usein kysytty kysymys 2",
+      "aikaleima": "2023-05-13T08:59:47.246Z",
+      "kommentit": [
+        {
+          "id": 15,
+          "tiketti": 11,
+          "lahettaja": 3,
+          "viesti": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sodales convallis purus. Etiam dui eros, vestibulum tincidunt lacus sed, pharetra luctus Leo. Sed nisl mauris, sodales cursus nisi a, mollis varius metus. Pellentesque enim mi, rhoncus eu tellus vitae, gravida porttitor ligula. Pellentesque efficitur vitae lectus pellentesque convallis. Sed et ultricies libero, in vulputate ipsum. Etiam aliquam dignissim sollicitudin. Interdum et malesuada fames ac ante ipsum primis in faucibus. Duis aliquet molestie est, a blandit mauris. Phasellus cursus libero molestie tincidunt mattis. Curabitur eu fermentum metus, suscipit commodo urna. Nulla facilisi. Nam nec efficitur odio, in consectetur felis.<br>Praesent eu ultricies nisl. Phasellus quis nisi eu orci facilisis fermentum et ut ipsum. Integer vitae posuere dolor. Vivamus nec mauris non neque pharetra eleifend. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Curabitur ac consequat est, ut aliquet massa. Morbi rutrum mi sit amet nibh suscipit, ut vulputate quam sollicitudin. Aenean a nibh at sapien porttitor eleifend. Maecenas id justo nec arcu lobortis pretium ut id diam. Mauris maximus auctor mauris. In varius dictum arcu, non eleifend arcu fermentum vel. Proin malesuada elit eros, nec laoreet nisl bibendum at.",
+          "aikaleima": "2023-05-12T08:59:47.261Z",
+          "tila": 1
+        },
+        {
+          "id": 16,
+          "tiketti": 11,
+          "lahettaja": 3,
+          "viesti": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sodales convallis purus. Etiam dui eros, vestibulum tincidunt lacus sed, pharetra luctus Leo. Sed nisl mauris, sodales cursus nisi a, mollis varius metus. Pellentesque enim mi, rhoncus eu tellus vitae, gravida porttitor ligula. Pellentesque efficitur vitae lectus pellentesque convallis. Sed et ultricies libero, in vulputate ipsum. Etiam aliquam dignissim sollicitudin. Interdum et malesuada fames ac ante ipsum primis in faucibus. Duis aliquet molestie est, a blandit mauris. Phasellus cursus libero molestie tincidunt mattis. Curabitur eu fermentum metus, suscipit commodo urna. Nulla facilisi. Nam nec efficitur odio, in consectetur felis.<br>Praesent eu ultricies nisl. Phasellus quis nisi eu orci facilisis fermentum et ut ipsum. Integer vitae posuere dolor. Vivamus nec mauris non neque pharetra eleifend. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Curabitur ac consequat est, ut aliquet massa. Morbi rutrum mi sit amet nibh suscipit, ut vulputate quam sollicitudin. Aenean a nibh at sapien porttitor eleifend. Maecenas id justo nec arcu lobortis pretium ut id diam. Mauris maximus auctor mauris. In varius dictum arcu, non eleifend arcu fermentum vel. Proin malesuada elit eros, nec laoreet nisl bibendum at.",
+          "aikaleima": "2023-05-12T08:59:47.262Z",
+          "tila": 1
+        }
+      ],
+      "kentat": []
+    }
+  ]
+
+}
