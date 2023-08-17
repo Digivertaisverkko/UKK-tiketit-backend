59cec2f2a37e077e028fb038ccbf25a95bffeda1
Erkki Tervo <erkki.tervo@oulu.fi>
2023-08-17 11:52:38 +0300

    Cleanup. Poistetaan tietokantaskriptit, koska migrations/sql sisältää samat skriptit

--- sql_reformatdvv.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo -u postgres psql -d "dvvukk" -f ./docs/postgres/dvvukk_drop_tables.txt 
-
-sudo -u postgres psql -d "dvvukk" -f ./docs/postgres/dvvukk_create_tables.txt
-
-sudo -u postgres psql -d "dvvukk" -f ./docs/postgres/dvvukk_sample_data.txt
